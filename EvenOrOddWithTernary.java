b5faf63e2c5502f11dc3703ea27b62f2e3200dc2
jpr-otter <96299330+jpr-otter@users.noreply.github.com>
2023-03-13 23:15:11 +0100

    Create EvenOrOddWithTernary.java

---
+++ EvenOrOddWithTernary.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class OddOrEven {
+
+    public static void main(String[] args) {
+      
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Give a number:");
+      
+        int evenOdd = Integer.valueOf(scan.nextLine());
+        String odd = ("Number " + evenOdd + " is odd.");
+        String even = ("Number " + evenOdd + " is even.");
+        String result;
+
+        result = (evenOdd % 2 > 0) ? odd : even;
+      
+        System.out.println(result);
+    }
+}
